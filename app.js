8777a684f8bd5d2804c230fa7de98a26ea1f8354
huyhoang9461 <huyhoang27102005@gmail.com>
2026-03-18 11:34:17 +0700

    cors

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(cors({ origin: 'https://sdn-frontend-huyhoang9461s-projects.vercel.app/' }));
+app.use(cors({ origin: 'https://sdn-frontend.vercel.app' }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
